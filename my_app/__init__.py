409b8e9494e538c96d87ecd6c476aa122318d3a7
Fors3cDream <xionx20@sina.cn>
2020-11-19 18:53:48 +0800

    Add blueprint

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+# app = Flask(__name__)
+db = SQLAlchemy(app)
 
 # Flask用这个秘钥保护用户会话，以防被篡改
 app.config['SECRET_KEY'] = 'hard to guess string' 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'database/data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'database/datas.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# 导入顺序 - 先初始化db 再进行导入即可成功
+from my_app.catalog.views import app_blue
+app.register_blueprint(app_blue)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from my_app.app import app
+from my_app import app
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
